8f02e7034caac163d2bdb6dfad4d99861f90da4a
Otavio Serra <31896900+olserra@users.noreply.github.com>
2019-07-19 12:09:56 +0100

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    consumer_key:         'trxgUtdbaKeYfypX5NLFW3ofj',
-    consumer_secret:      'vowE6Jghr55FXDPU2vyjkUpOZgfOcAjgyFyGpBLSvQn5eT6p9p',
-    access_token:         '2991073630-Ohez5v9pIt9QJxSceDzuEm8uGLbeS7g9aPyRCnl',
-    access_token_secret:  'IeU8hNwzg9ODRMJcNcWQqoKHwNBV8w3FR8XXuEW5nmcBs',
-}
